b6b0fe7a809229cdfa7187008750685d78af75e1
DanielRumay <165932669+DanielRumay@users.noreply.github.com>
2024-09-15 17:59:39 +0000

    Proyect

--- alerta.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-include "./CODE/inc/head.php";
-include "./CODE/vistas/404.php";
-include "./CODE/inc/footer.php";
--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include "./CODE/inc/head.php";
-$_SESSION['nombre'] = "";
-include "./CODE/inc/navbar.php";
-include "./CODE/vistas/login.php";
-include "./CODE/inc/footer.php";
